752d9b98ec3367d55d134ed8ffa8e7ac7a196427
anna1568 <Mangle1568@gmail.com>
2025-09-07 21:58:35 +0800

    Поделала шаблоны и вьюсеты

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from music import views
+
 urlpatterns = [
+    path('', views.ShowStudentsView.as_view()),
     path('admin/', admin.site.urls),
 ]
--- music/views.py
+++ music/views.py
@@ -1,3 +1,17 @@
+from typing import Any
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from music.models import Student
 
 # Create your views here.
+class ShowStudentsView(TemplateView):
+    template_name = "music/show_students.html"
+
+    def get_context_data(self, **kwargs : Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['music'] = Student.objects.all()
+
+        return context
+
